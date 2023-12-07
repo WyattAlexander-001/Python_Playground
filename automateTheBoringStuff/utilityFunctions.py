3521bebdab37cc39f4e5769a1c03dc52e062aeac
WyattAlexander-001 <wyattbushman117@gmail.com>
2023-12-07 00:09:43 -0500

    added moving script

--- automateTheBoringStuff/utilityFunctions.py
+++ automateTheBoringStuff/utilityFunctions.py
@@ -1,3 +1,7 @@
 import shutil
-shutil.copy('dummyFolder/copyMe.ruby', 'automateTheBoringStuff') # copy file and paste it in the given directory
-shutil.copy('dummyFolder/copyMe.ruby', 'automateTheBoringStuff/renamed.ruby') 
+#s hutil.copy('dummyFolder/copyMe.ruby', 'automateTheBoringStuff') # copy file and paste it in the given directory
+# shutil.copy('dummyFolder/copyMe.ruby', 'automateTheBoringStuff/renamed.ruby') 
+
+# shutil.copytree('dummyFolder', 'dummyFolderBackup') # copy folder and paste it in the given directory
+
+shutil.move('automateTheBoringStuff/renamed.ruby', 'dummyFolder') # move file to the given directory
